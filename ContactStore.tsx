3b46ef4c93a9c49e277e94b45455369eff1727fe
Kuba Hofman <126712459+Mithynite@users.noreply.github.com>
2026-02-24 12:12:20 +0100

    Add consent field and update ContactState interface

--- ContactStore.tsx
+++ ContactStore.tsx
@@ -6,31 +6,39 @@ import { create } from 'zustand'
   phoneNumber: string
   comment: string
   file: File | null
+  consent: boolean
   setField: (field: string, value: any) => void
   resetForm: () => void
 }*/
 
+// TODO přidat dynamicky další fieldy, defaultně tu byly fullname, email atd. ale je třeba sem dát jen
+// 
 interface ContactState {
-  fields: Record<string,any>,
+  fields: Record<string, any>,
+  //file: File | null,
+  //consent: boolean,
   initializeFields:(fields: Record<string,any>) => void,
   setField: (field: string, value: any) => void,
-  resetForm: () => void
+  //setFileField:(field:string, value:any) => void,
+  resetForm: () => void,
 }
 
 export const useContactStore = create<ContactState>((set) => ({
   fields: {},
+  //file: null,
   initializeFields:(incomingFields) => set({
     fields:incomingFields,
   }),
-  setField: (field, value) => 
-    set((state) => ({ 
-      fields:{
-        ...state, 
-        [field]: value 
-      }
-    })),
-  resetForm: () =>
+  setField: (field, value) => set((state) => ({ ...state, 
+    fields:{
+      ...state, 
+      [field]: value
+    }
+  })),
+  //setFileField:(field, value) => set((state) => ({...state, [field]:value})),
+  resetForm:() =>
     set({
-      fields:{}
+      fields:{},
+      //file: null,
     }),
 }))
