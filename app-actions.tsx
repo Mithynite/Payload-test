4aa694eea9dccc6201df89393b18c5a87b5a091a
Kuba Hofman <126712459+Mithynite@users.noreply.github.com>
2026-02-26 12:30:25 +0100

    Refactor form validation and field handling

--- app-actions.tsx
+++ app-actions.tsx
@@ -3,12 +3,12 @@
 import config from '@/payload.config'
 import { getPayload } from 'payload'
 import * as v from 'valibot'
-import { IActionResult } from '../types/IActionResult'
 import { Form } from '@/payload-types'
 import { BaseIssue, PipeItem } from 'valibot'
-import { getFieldName } from '../helpers/form-fields-helper'
+import { buildFileTypeString, mapAcceptableFileTypes } from '../helpers/file-information-helper'
 
 type FormField = NonNullable<Form["fields"]>[number]
+type MimeType = `${string}/${string}`;
 
 export async function $sendApplicationForm({
   formData,
@@ -16,45 +16,76 @@ export async function $sendApplicationForm({
 }: {
   formData: FormData,
   initialFields: FormField[]
-}): Promise<IActionResult<undefined>> {
+}) {
+
+  function getProperty<T extends keyof FormField>(field:FormField, key: T, defaultValue: any){
+    const property = key in field && field[key] ? field : defaultValue
+    return property;
+  }
 
   const constructFieldSchema = (field : FormField) => {
     const rules : PipeItem<any, string, BaseIssue<unknown>>[] = [];
-    let base : any = v.string(); // TODO: get rid of "any"
+    let base;
 
-    if(field.blockType === "checkbox"){
-      base = v.boolean()
-    }else if(field.blockType === "upload"){
-      base = v.file()
-    }
+    const fname : (string | null) = "name" in field && field.name ? field.name : null
 
-    // TODO: create generic function for this
+    const required = getProperty(field, "required", false)
+    const isPhoneNumber = getProperty(field,"isPhoneNumber", false)
+    const label = getProperty(field, "label", "")
+    const maxLength = getProperty(field, "maxLength", null)
+    const fileSize = getProperty(field, "fileSize", null)
+
     const blockType : string = field.blockType
-    const fname : (string | null) = getFieldName(field)
+    switch (field.blockType) {
+      case "text":
+      case "textarea":
+        base = v.string()
+          if(isPhoneNumber)
+            break
+          
+          // Required
+          if(required)
+            rules.push(v.nonEmpty(`Vyplňte ${label}`))
+          // Max Length
+          if(maxLength){
+            rules.push(v.maxLength(maxLength, `Zadaná hodnota ${label} je moc dlouhá`))
+          }
+        break
 
-    const required : boolean = "required" in field && field.required ? field.required : false
-    const label : string = "label" in field && field.label ? field.label : ""
-    const maxLength = "maxLength" in field && field.maxLength ? field.maxLength : null
-    const fileSize = "fileSize" in field && field.fileSize ? field.fileSize : null
+      case "email":
+        base = v.string()
 
-    /**
-     * Text:
-     *  - string
-     * 
-     *  - required
-     *  - maxlength
-     */
+        // Required
+        if(required)
+          rules.push(v.nonEmpty(`Vyplňte ${label}`))
+        // Email
+        rules.push(v.email('E-mail je ve špatném formátu'))
+        // Max Length
+        if(maxLength)
+          rules.push(
+            v.maxLength(
+            254,
+            'Vaše emailová adresa přesahuje maximální délku emailu podle specifikace RFC 3696',
+          ))
+        break
+      case "checkbox":
+        base = v.boolean()
 
-    if(required){
-      rules.push(v.nonEmpty(`Vyplňte ${label}`))
-    }
+        // Required
+        if(required)
+          rules.push(v.check((input) => input))
+        break
 
-    if(blockType === "email"){
-      rules.push(v.email('E-mail je ve špatném formátu'))
-    }
+      case "upload":
+        base = v.file('Prosím vyberte soubor')
+        const mimeTypes = getProperty(field,"mimeTypes",[] as MimeType[])
+        const acceptableFileTypes = mapAcceptableFileTypes(mimeTypes)
+        const fileTypeString = buildFileTypeString(mimeTypes)
 
-    if(maxLength){
-      rules.push(v.maxLength(maxLength, `Hodnota ${label} je moc dlouhá`))
+        rules.push(v.mimeType(acceptableFileTypes, fileTypeString))
+
+      default:
+        return null
     }
 
     if(fileSize){
@@ -64,6 +95,7 @@ export async function $sendApplicationForm({
     return v.pipe(base, ...rules);
   }
 
+  /** Done */
   const rawFormDataMapped = Object.fromEntries(
     initialFields.map(f => {
       const fname = f.name
@@ -113,7 +145,7 @@ export async function $sendApplicationForm({
     comment: v.pipe(v.string(), v.nonEmpty('Vyplňte komentář')),
     consent: v.pipe(
       v.boolean(),
-      v.check((input) => input),
+      v.check((input) => input), // input must be true
     ),
     file: v.pipe(
       v.file('Prosím vyberte soubor'),
@@ -142,6 +174,7 @@ export async function $sendApplicationForm({
   try {
     const payload = await getPayload({ config })
 
+    /** Extract all phone number fields and all file-upload fields to work with them separately */
     const { phonenumber, file, ...data } = validatedFields.output
 
     const arrayBuffer = await file.arrayBuffer()
